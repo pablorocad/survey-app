852daa1d9421243c8d183644f48e8064ef8bddac
Pablo Roca <3014066260101@ingenieria.usac.edu.gt>
2021-05-26 21:17:27 -0600

    Nodemon and router

--- backend/src/routes/SurveyRouter.js
+++ backend/src/routes/SurveyRouter.js
@@ -2,22 +2,16 @@
 //Imports===============================================================================
 //======================================================================================
 const express = require('express');
-const surveyRouter = require('./routes/SurveyRouter');
 
 //======================================================================================
 //Configuration=========================================================================
 //======================================================================================
-const app = express();
-const PORT = 3000;
+const router = express.Router();
+const URL = '/survey'
 
-app.set('port',PORT);
 
-app.get('/',(req,res) => {
-    res.send('Server Running');
-})
-
-app.use(surveyRouter);
-
-app.listen(PORT, () => {
-    console.log(`Survey app listening on port: ${app.get('port')}`);
+router.get(URL,(req,res) => {
+    res.send('Surveys')
 });
+
+module.exports = router;
