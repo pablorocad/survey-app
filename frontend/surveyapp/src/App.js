290deecb6651d075f2b0376d3240f453ffa7c73b
Pablo Roca <3014066260101@ingenieria.usac.edu.gt>
2021-05-28 15:39:07 -0600

    Usin foundation

--- frontend/surveyapp/src/App.js
+++ frontend/surveyapp/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import CreateSurvey from './components/createSurvey/CreateSurvey';
 
 function App() {
   return (
     <div className="App">
-      
+      <CreateSurvey></CreateSurvey>
     </div>
   );
 }
---
+++ frontend/surveyapp/src/components/createSurvey/CreateSurvey.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button, Colors } from 'react-foundation';
+
+class CreateSurvey extends Component {
+
+    render(){
+        return (
+            <Button color={Colors.SUCCESS}>Submit</Button>
+        )
+    }
+
+}
+
+export default CreateSurvey;
