3ba5f228d46e4c69a64107cf11e788fb9f40b5a4
Pablo Roca <3014066260101@ingenieria.usac.edu.gt>
2021-05-28 17:44:40 -0600

    Using Bootstrap

--- frontend/surveyapp/src/components/createSurvey/CreateSurvey.js
+++ frontend/surveyapp/src/components/createSurvey/CreateSurvey.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import { Button, Colors } from 'react-foundation';
+import './CreateSurvey.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 class CreateSurvey extends Component {
 
     render(){
         return (
-            <Button color={Colors.SUCCESS}>Submit</Button>
+            <div className="d-flex justify-content-center">
+                <div className="container-form">
+                    <Form>
+                        <Form.Group controlId="exampleForm.ControlInput1">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="name@example.com" />
+                        </Form.Group>
+                    </Form>
+                    <div className="d-flex justify-content-center">
+                        <Button variant="success">Success</Button>{' '}
+                    </div>
+                </div>
+        
+            </div>
         )
     }
 
